4ef85581c5079b27c69687cc606094652f824667
objektwerks <objektwerks@runbox.com>
2018-10-02 15:39:21 -0400

    just trying to get a simple scala.js program to work!

--- src/main/scala/todo/TodoApp.scala
+++ src/main/scala/todo/TodoApp.scala
@@ -4,9 +4,9 @@ import org.scalajs.dom._
 
 object TodoApp {
   def main(args: Array[String]): Unit = {
-    val paragraph = window.document.createElement("p")
+    val p = document.createElement("p")
     val text = document.createTextNode("Todo!")
-    document.body.appendChild(paragraph.appendChild(text))
+    document.body.appendChild(p.appendChild(text))
     ()
   }
 }
