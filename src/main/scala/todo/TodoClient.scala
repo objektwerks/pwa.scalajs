a807b9d8813374717a53258900cf60ac2b79d2b2
objektwerks <objektwerks@runbox.com>
2018-10-04 11:52:43 -0400

    removed scala.js annotations in TodoClient

--- src/main/scala/todo/TodoClient.scala
+++ src/main/scala/todo/TodoClient.scala
@@ -2,12 +2,7 @@ package todo
 
 import org.scalajs.dom._
 
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
-
-@JSExportTopLevel("TodoClient")
 object TodoClient{
-
-  @JSExport
   def main(args: Array[String]): Unit = {
     val content = document.getElementById("content")
     val p = document.createElement("p")
