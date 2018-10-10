7254bfcf1922ed09237ee63a12c7234ec8739851
objektwerks <objektwerks@runbox.com>
2018-10-10 13:26:25 -0400

    implemented js todo rest client circe support

--- js/src/main/scala/todo/TodoRestClient.scala
+++ js/src/main/scala/todo/TodoRestClient.scala
@@ -12,19 +12,19 @@ class TodoRestClient(todosUrl: String) {
   val headers = Map("Content-Type" -> "application/json; charset=utf-8", "Accept" -> "application/json")
 
   def listTodos(): Future[List[Todo]] = Ajax.get(url = todosUrl, headers = headers).map { xhr =>
-    xhr.responseText.asJson.as[List[Todo]].toOption.get
+    xhr.responseText.asJson.as[List[Todo]].getOrElse(List.empty[Todo])
   }
 
   def addTodo(todo: Todo): Future[Id] = Ajax.post(url = todosUrl, headers = headers, data = todo.asJson.toString).map { xhr =>
-    xhr.responseText.asJson.as[Id].toOption.getOrElse(Id(0))
+    xhr.responseText.asJson.as[Id].getOrElse(Id(0))
   }
 
   def updateTodo(todo: Todo): Future[Count] = Ajax.put(url = todosUrl, headers = headers, data = todo.asJson.toString).map { xhr =>
-    xhr.responseText.asJson.as[Count].toOption.getOrElse(Count(0))
+    xhr.responseText.asJson.as[Count].getOrElse(Count(0))
   }
 
   def removeTodo(todo: Todo): Future[Count] = Ajax.delete(url = todosUrl, headers = headers, data = todo.asJson.toString).map { xhr =>
-    xhr.responseText.asJson.as[Count].toOption.getOrElse(Count(0))
+    xhr.responseText.asJson.as[Count].getOrElse(Count(0))
   }
 }
 
